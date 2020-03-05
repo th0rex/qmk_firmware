9e265d79fce13e15505268b61de14a572a98b107
th0rex <c0untlizzi@gmail.com>
2020-03-05 22:52:40 +0100

    Add some disabled keybindings again
    Would be nice if we could compile on nix without compiling
    all of fucking avr-gcc first.

--- keyboards/ergodox_ez/keymaps/th0rex/keymap.c
+++ keyboards/ergodox_ez/keymaps/th0rex/keymap.c
@@ -18,10 +18,10 @@ enum custom_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = LAYOUT_ergodox(
       /* Left Half */
-      KC_ESC,  KC_1,         KC_2,    KC_3,    KC_4,   KC_5, KC_NO/* KC_MEDIA_PLAY_PAUSE */,
-      KC_TAB,  KC_Q,         KC_W,    KC_E,    KC_R,   KC_T, KC_NO/*KC_MEDIA_NEXT_TRACK */,
+      KC_ESC,  KC_1,         KC_2,    KC_3,    KC_4,   KC_5, KC_MEDIA_PLAY_PAUSE,
+      KC_TAB,  KC_Q,         KC_W,    KC_E,    KC_R,   KC_T, KC_MEDIA_NEXT_TRACK,
       DE_CIRC, KC_A,         KC_S,    KC_D,    KC_F,   KC_G,
-      KC_LSFT, LALT_T(DE_Y), KC_X,    KC_C,    KC_V,   KC_B, KC_NO/*KC_MEDIA_PREV_TRACK */,
+      KC_LSFT, LALT_T(DE_Y), KC_X,    KC_C,    KC_V,   KC_B, KC_MEDIA_PREV_TRACK,
       KC_LCTL, OSL(1),       DF(1),   KC_LALT, KC_RALT,
 
       /* Thumb cluster */
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_SPC, KC_BSPC, KC_END,
 
       /* Right Half */
-      KC_NO/*KC_PSCR*/,   KC_6, KC_7, KC_8,    KC_9,   KC_0,            KC_BSPC,
+      KC_PSCR,   KC_6, KC_7, KC_8,    KC_9,   KC_0,            KC_BSPC,
       DE_SS,     DE_Z, KC_U, KC_I,    KC_O,   KC_P,            DE_PLUS,
       /*      */ KC_H, KC_J, KC_K,    KC_L,   DE_HASH,         KC_ENT,
       DE_LESS,   KC_N, KC_M, DE_COMM, DE_DOT, RCTL_T(DE_MINS), KC_RSFT,
